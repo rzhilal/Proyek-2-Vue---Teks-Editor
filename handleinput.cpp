7e0eba20ff3ba39cd6a4d4059f7f00f9cf8294a1
Rizq Hilal Rifaasya <rizq.hilal.tif21@polban.ac.id>
2022-03-24 16:55:56 +0700

    Handle enter dan backspace

--- handleinput.cpp
+++ handleinput.cpp
@@ -16,18 +16,39 @@ void editorKeyPresses ()
 				{	
 					if(matriks.numcols <= 0) //kondisi ketika berada di posisi awal
 					{
-						
+						matriks.numrows--;
+						if(matriks.numrows <= 0)
+						{
+							matriks.numrows = 0;
+						}
+						matriks.numcols = strlen(matriks.teks[matriks.numrows]) - 1;
+						matriks.teks[matriks.numrows][matriks.numcols] = '\0';
+						system("cls");
+						for(int i=0; i<= matriks.numrows;i++)
+						{
+							if(i != matriks.numrows)
+							{
+								printf("%s\n", matriks.teks[i]);
+							}
+							else
+							{
+								printf("%s", matriks.teks[i]);
+							}
+						}
 					}
 					else //kondisi berada ditengah maupun akhir
 					{
-						
+						matriks.teks[matriks.numrows][matriks.numcols] = '\0';
+						matriks.numcols--;
+						printf("\b \b");
 					}
 				}
 				
 				/***Handle Enter***/
 				else if(matriks.teks[matriks.numrows][matriks.numcols] == 13)
 				{
-					
+					matriks.teks[matriks.numrows][matriks.numcols] = '\0';
+					break;
 				}
 				
 				/*** Shortcut key ***/
