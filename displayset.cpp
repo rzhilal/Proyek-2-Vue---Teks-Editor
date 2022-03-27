4be2f47be0be47c384d54ac5ca3d690e67393a83
Zahratul Mardiyah <98735694+zhrtlmrdyh@users.noreply.github.com>
2022-03-27 18:06:28 +0700

    Title dan tampilan_awal

--- displayset.cpp
+++ displayset.cpp
@@ -2,5 +2,27 @@
 
 void displaySet()
 {
+	TCHAR szOldTitle[MAX_PATH];
+   	TCHAR szNewTitle[MAX_PATH];
+   	
+   	// Save current console title.
 	
+	if( GetConsoleTitle(szOldTitle, MAX_PATH) )
+	{
+		// Build new console title string.
+	
+		StringCchPrintf(szNewTitle, MAX_PATH, TEXT("Vue - Text Editor"), szOldTitle);
+	
+	    // Set console title to new title
+	    if( !SetConsoleTitle(szNewTitle) );
+	}
 }
+
+void tampilan_awal()
+{
+	printf("\n\n\n\n\n\n\n\n\t\tVUE - TEKS EDITOR");
+	printf("\n\tCtrl+E untuk panduan dan shorcut\n\n\n\n\n\n\n\n\n\n\n");
+	system("pause");
+	system("cls");
+}
+
