473235378712a7010311d0f63188c1d3a406ebd4
Jhaposans29 <jeremia.haposan.tif21@polban.ac.id>
2022-03-27 14:24:10 +0700

    modul find

--- freplace.cpp
+++ freplace.cpp
@@ -1,38 +1,25 @@
 #include "kota.H"
 
-int n;	// number of string in the array
-int i; // deklarasi variabel i 
-
-void find()
+void find(char *word)
 {
-	// input string yang di cari
-	printf("Input nama character untuk menentukan baris: ");
-	scanf("%s", matriks.teks);
-
-	// mengecek nilai nya sama atau tidak dalam string compare
-	for (i = 0; i < n; ++i) {
-		if (!strcmp(matriks.teks[matriks.numrows][matriks.numcols]) {
-			break;
+	int row[MAXR];
+	int j = 0;
+	for (int i = 0; i <= matriks.numrows ; ++i) {
+		if (!strcmp(matriks.teks[i], word))
+		{
+			row[j] = i;
+			j++;
 		}
 	}
-}
-
-void replace()
-{
-	// mengecek nilai index dengan variabel inputan user yaitu n karena tidak sama atau false
-	if (i != n) {
-		printf("Masukan kata :");
-		scanf("%s", matriks.teks[matriks.numrows][matriks.numcols]);
-		printf("\n\n");
-		
-	
-//		printf("\n\n %s is present in the array at row %d", str, i);
-	// pendeklarasian variabel j untuk menampilkan hasil dari inputan i 
-		int j;
-		for (j = 0; j < n; ++j) {
-		printf("%s \n", s[j]);
-		}
-	}else {
-		printf("%s is not present in the array", matriks.teks[matriks.numrows][matriks.numcols]);
+	if(j != 0)
+	{
+		printf("\n\n\t Baris ke : ");
+		for(int i = 0; i<j; i++)
+		{
+			printf("%d, ", row[i]+1);
+		}		
+	} else
+	{
+		printf("\n\n\tKata tidak ada");
 	}
 }
