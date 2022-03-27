d4243bd1e47816a3e32dcf319cc34e33f8811eb2
salmanalfarisi29 <salmannalfarisii994@gmail.com>
2022-03-27 19:55:04 +0700

    modul statbar

--- cursorinteract.cpp
+++ cursorinteract.cpp
@@ -7,5 +7,8 @@ void curInteraction()
 
 void curStat()
 {
-	
+	system("cls");
+	printf("\n\n\tLine : %d || Kolom : %d\n\n", matriks.numrows+1, matriks.numcols+1);
+	system("pause");
+	system("cls");
 }
