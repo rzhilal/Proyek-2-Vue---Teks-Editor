793b6c9d32223d8955330148d7b6223793a597b5
Zahratul Mardiyah <98735694+zhrtlmrdyh@users.noreply.github.com>
2022-03-27 14:59:11 +0700

    OpenFile
    Belum ada pengecekkan error

--- filehandling.cpp
+++ filehandling.cpp
@@ -7,5 +7,19 @@ void editorSaveFile(char *fname)
 
 void editorOpenFile(char *fname)
 {
+	FILE *fptr = NULL;
+
+    fptr = fopen(fname, "r");
+    
+	matriks.numrows = 0;
 	
+	while(fgets(matriks.teks[matriks.numrows], MAXC, fptr)) 
+	{
+        matriks.teks[matriks.numrows][strlen(matriks.teks[matriks.numrows]) - 1] = '\0';
+        matriks.numrows++;
+    }
+    
+	editorPrint();  
+    fclose(fptr);
 }
+
